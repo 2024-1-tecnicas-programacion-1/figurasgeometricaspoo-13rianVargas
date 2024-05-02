019d4fd291fd068dae6fc057a99ad3a0f65ad4dd
13rian Vargas <brianstevenvargasclavijo@gmail.com>
2024-05-02 15:37:35 -0500

    Se agregan atributos y métodos de la clase hija triángulo.
    Se agrega también para el caso del triangulo isósceles y rectángulo.

--- src/main/java/com/mycompany/figurasgeometricaspoo/Triangulo.java
+++ src/main/java/com/mycompany/figurasgeometricaspoo/Triangulo.java
@@ -1,5 +1,24 @@
 package com.mycompany.figurasgeometricaspoo;
 
 public class Triangulo {
-    
-}
+    private double base;
+    private double altura;
+
+    public Triangulo(double base, double altura){
+        this.base = base;
+        this.altura = altura;
+    }//cierra constructor
+
+    public double obtenerArea(){
+        return (base * altura)/2;
+    }//cierra obtenerArea
+
+    public double obtenerPerimetro(){
+        double hip = Math.sqrt(base/2) + Math.sqrt(altura);
+        double iso = base + hip + hip;
+        double rec = base + altura+ hip;
+
+        return "Isósceles: " + iso + " Rectángulo: " + rec;
+    }//cierra obtenerPerimetro
+
+}//cierra class
